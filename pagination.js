a1feb56d2e77dc88700933f8c5f1068c1cf517e3
mingzepeng <mingzepeng@gmail.com>
2014-08-04 15:11:01 +0800

    构造函数添加config参数 完善test.html

--- pagination.js
+++ pagination.js
@@ -10,7 +10,7 @@
 })(this, function() {
 	'use strict';
 
-	function Pagination(totalRow,pageSize) {
+	function Pagination(totalRow,pageSize,config) {
 		this.config = {
 			offset : 2,
 			omission : '...',
@@ -20,6 +20,7 @@
 		this.totalRow = !isNaN(totalRow) ? parseInt(totalRow) : 0;   //总的行数
 		this.pageSize = !isNaN(pageSize) ? parseInt(pageSize) : 10;  //一页数据的条数
 		this.totalPage = Math.ceil(totalRow/pageSize);         //计算出总的页数
+		this.setConfig(config);
 		this.pageNumber = 1;									 //当前页
 	}
 
@@ -30,23 +31,37 @@
 			throw new Error("pageNumber is higher than the totalPage");
 
 		var o = this.config.offset,pages = [],i=0;
+
+		//页数不为1，则插入pre
 		if(pageNumber > 1) pages.push(this.config.pre);
+
+		//对当前页前半部分进行判断
 		if(pageNumber - 2 * o - 1 <= 1 ) {
 			for (i = 1; i < pageNumber; i++) pages.push(i);
 		}else{
+			//期间页数太多,则添加省略号
 			for (i = 1; i <= o; i++) pages.push(i);
 			pages.push(this.config.omission);
 			for (i = pageNumber - o; i < pageNumber; i++) pages.push(i);	
 		}
+		
+		//插入当前页
 		pages.push(pageNumber);
+		
+		//对当前页后半部分进行处理
 		if(this.totalPage - pageNumber > 2 * o + 1 ){
+			//期间页数太多,则添加省略号
 			for (i = pageNumber+1; i <= pageNumber + o; i++) pages.push(i);
 			pages.push(this.config.omission);
 			for (i = this.totalPage - o + 1; i <= this.totalPage; i++) pages.push(i);	
 		}else{
 			for (i = pageNumber+1; i <= this.totalPage; i++) pages.push(i);
 		}
+
+		//如果当前页数非最后一页,则插入next
 		if(pageNumber < this.totalPage) pages.push(this.config.next);
+
+
 		return {
 			pageNumber : pageNumber,
 			pageSize : this.pageSize,
